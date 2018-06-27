75756ca59da1a6b282a8ff7f37d46451fcc62013
Byron Friesen <bfriesen@malin.local>
2018-06-27 13:04:41 -0500

    cloud-init takes care of sudo, remove docker

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -eux
 
+# install cloud-init
+yum -y install cloud-init
+
 # change cloud-init user name
 sed -i 's/name: fedora/name: ec2-user/g' /etc/cloud/cloud.cfg
 sed -i 's/gecos: Fedora Cloud User/gecos: EC2 Default User/g' /etc/cloud/cloud.cfg
 sed -i 's/distro: fedora/distro: rhel/g' /etc/cloud/cloud.cfg
-
-# use first grub option
-grub2-set-default 1
-grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # lock login user
 passwd -l ec2-user
 
 # delete cloud-init lock
 rm -rf /var/lib/cloud/instance/sem/*
+
+# remove UEK kernel as AWS does not import them and remake grub menu just in case
+yum -y remove kernel-uek-4.1.12 kernel-uek-firmware
+grub2-mkconfig -o /boot/grub2/grub.cfg
+ 
+
